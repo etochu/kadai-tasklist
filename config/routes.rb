d3f62bf8fbc6fbca4c7f10c2ea1eb9508daf6cb7
etochu <pixies-wave_of-mutilation@ezweb.ne.jp>
2017-08-29 07:46:14 +0000

    top page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'toppages/index'
+
   root to: 'tasks#index'
 
-  resources :tasks
+   get 'signup', to: 'users#new'
+  resources :users,only: [:index, :show, :new, :create]
 end
