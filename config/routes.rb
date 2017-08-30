1c4b2f93ec121a8dd97908173425800ccd7d52e5
etochu <pixies-wave_of-mutilation@ezweb.ne.jp>
2017-08-30 05:38:47 +0000

    post

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  get 'toppages/index'
-
   root to: 'tasks#index'
+  
+  get 'login',to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
+  get 'signup', to: 'users#new'
 
-   get 'signup', to: 'users#new'
-  resources :users,only: [:index, :show, :new, :create]
+  resources :users, only: [:index, :show, :new, :create]
+  resources :tasks
 end
